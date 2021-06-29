4146b4c0dad8513945badd9970c26764f705af3f
jose <jmolina721@gmail.com>
2021-06-29 11:18:05 -0500

    Added middleware for routes

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// parse json body for routes
+app.use(express.json());
+
+// turn on routes
+app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
